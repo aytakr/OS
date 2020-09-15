b03fcff5167b749066053363dae600771c82161b
dd-fig <ziqunion95@gmail.com>
2020-09-16 02:22:07 +0900

    MSCH

--- Lab1/main.sh
+++ Lab1/main.sh
@@ -1,37 +1,48 @@
 #! /usr/bin/env bash
+function noFileError {
+  exit -1
+}
+function accessError {
+  exit -2
+}
+function argumentError {
+  exit -3
+}
+function mathError {
+  exit -4
+}
 case $1 in
 calc)
   if ! [[ -e $1 ]]
   then
-    echo "Ошибка - нет файла calc"
-    exit 0
+    echo "Ошибка - нет файла $1"
+    noFileError
   fi
   if ! [[ -r $1 ]]
   then
-    echo "Ошибка - недостаточно прав для запуска calc"
-    exit 0
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
   fi
   if [[ $# -ne 4 ]]
   then
     echo "Ошибка - неправильное количество аргументов. После $1 введите sum/sub/mul/div и два целых числа"
-    exit 0
+    argumentError
   fi
   re='^[0-9]+$'
   if ! [[ "$3" =~ $re && "$4" =~ $re ]]
   then
     echo "Ошибка - Неверный третий или четверный аргумент. Выберите в качестве третьего и четвертого аргументов целое число"
-    exit 0
+    argumentError
   fi
   if [[ "$2" = "div" && $4 -eq 0 ]]
   then
     echo "Ошибка - деление на ноль невозможно"
-    exit 0
+    mathError
   fi
   if [[ "$2" != "sum" && "$2" != "sub" && "$2" != "mul" && "$2" != "div" ]]
   then
     echo "Ошибка - Неверный второй аргумент. Выберите вторым аргументом sum/sub/mul/div"
-    echo "$2"
-    exit 0
+    argumentError
   fi
   . ./calc
   res=$($2 $3 $4)
@@ -39,18 +50,42 @@ calc)
   exit 0
 ;;
 search)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 3 ]]
   then
     echo "Ошибка - неправильное количество аргументов. После $1 введите название директории и регулярное выражение"
-    exit 0
+    argumentError
   fi
+  cd /home/michil/
+  ls | grep Documents
+  echo `pwd`
+  #. ./search
   exit 0
 ;;
 reverse)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 3 ]]
   then
     echo "Ошибка - неправильное количество аргументов. После $1 введите названия двух файлов"
-    exit 0
+    argumentError
   fi
   files=($(ls))
   flag1=0
@@ -69,56 +104,120 @@ reverse)
   if [[ $flag1 -ne 1 || $flag2 -ne 1 ]]
   then
     echo "Ошибка - таких файлов нет в директории."
-    exit 0
+    noFileError
   fi
   . ./reverse
   $(rev $2 $3)
   exit 0
 ;;
 strlen)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 2 ]]
   then
     echo "Ошибка - неправильное количество аргументов. После strlen введите строку"
-    exit 0
+    argumentError
   fi
   . ./strlen
   echo "$(strlen $2)"
   exit 0
 ;;
 log)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 1 ]]
   then
     echo "Ошибка - неправильное количество аргументов. Параметр $1 не имеет дополнительных аргументов"
-    exit 0
+    argumentError
   fi
   exit 0
 ;;
 exit)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 1 ]]
   then
     echo "Ошибка - неправильное количество аргументов. Параметр $1 не имеет дополнительных аргументов"
-    exit 0
+    argumentError
   fi
-  exit 0
+  exit
 ;;
 help)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 1 ]]
   then
     echo "Ошибка - неправильное количество аргументов. Параметр $1 не имеет дополнительных аргументов"
-    exit 0
+    argumentError
   fi
-  . ./menu
-  menu
+  . ./help
+  help
   exit 0
 ;;
 interactive)
+  if ! [[ -e $1 ]]
+  then
+    echo "Ошибка - нет файла $1"
+    noFileError
+  fi
+  if ! [[ -r $1 ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска $1"
+    accessError
+  fi
   if [[ $# -ne 1 ]]
   then
     echo "Ошибка - неправильное количество аргументов. Параметр $1 не имеет дополнительных аргументов"
-    exit 0
+    argumentError
   fi
   . ./interactive
   interactive
   exit 0
+;;
+*)
+  if ! [[ -e "help" ]]
+  then
+    echo "Ошибка - нет файла help"
+    noFileError
+  fi
+  if ! [[ -r "help" ]]
+  then
+    echo "Ошибка - недостаточно прав для запуска help"
+    accessError
+  fi
+  . ./help
+  help
 ;;
 esac
